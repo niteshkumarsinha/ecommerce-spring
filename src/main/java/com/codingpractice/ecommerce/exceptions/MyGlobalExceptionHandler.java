1bb5d18c108e4ccde393b0824b49b65d0095b6e7
NITESH KUMAR <nitesh111sinha@gmail.com>
2026-05-07 18:55:33 +0530

    api response

--- src/main/java/com/codingpractice/ecommerce/exceptions/MyGlobalExceptionHandler.java
+++ src/main/java/com/codingpractice/ecommerce/exceptions/MyGlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.codingpractice.ecommerce.exceptions;
 
 
+import com.codingpractice.ecommerce.dto.APIResponse;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
@@ -29,15 +30,17 @@ public class MyGlobalExceptionHandler {
     }
 
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e){
+    public ResponseEntity<APIResponse> myResourceNotFoundException(ResourceNotFoundException e){
         String message = e.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+        APIResponse apiResponse = new APIResponse(message, false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
     }
 
 
     @ExceptionHandler(APIException.class)
-    public ResponseEntity<String> myAPIException(APIException ex){
+    public ResponseEntity<APIResponse> myAPIException(APIException ex){
         String message = ex.getMessage();
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        APIResponse apiResponse = new APIResponse(message, false);
+        return new ResponseEntity<>(apiResponse, HttpStatus.BAD_REQUEST);
     }
 }
